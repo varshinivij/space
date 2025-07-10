f537e121f0abb1210c7e13681fa239f01f29d402
Varshini Vijay <varshinivijay05@gmail.com>
2025-07-10 12:14:37 -0700

    testing out branches

--- project_builder/src/App.jsx
+++ project_builder/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css';
+import Sample from './sample';
+import Sample2 from './Sample2';
 
 function App() {
-  return (<main>
-      <h1>Welcome to the Project Builder Application</h1>
-      <h2>This is my first-application so I am very nervous</h2>
-      <p id="user">What would you like to build? <button> Submit! </button> </p>
-      <p id="output"> Steps you can take: </p>
-    </main> );
+  return (
+  <> 
+    {/* <Sample/> */}
+    <Sample2/> 
+  </>
+  );
 }
 
 export default App;
