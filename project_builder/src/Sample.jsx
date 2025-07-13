37c8cf548f9d235ea4d3b8427f8cc618c483502d
Varshini Vijay <varshinivijay05@gmail.com>
2025-07-13 14:24:30 -0700

    trying t make it run

--- project_builder/src/Sample.jsx
+++ project_builder/src/Sample.jsx
@@ -1,22 +1,25 @@
-import './index.css'
-import {useState} from 'react';
+import './index.css';
+import { useState } from 'react';
 
-function Sample(){
-
+function Sample() {
   const [state, setState] = useState(false);
 
-  function handleOnClick( {target} ){
+  function handleOnClick({ target }) {
     console.log(`Button was ${target.value}`);
-    setState((prev) => !prev); 
+    setState((prev) => !prev);
   }
-  
-  return (<div>
+
+  return (
+    <div>
       <h1>Welcome to the Project Builder Application</h1>
-      <h2>This is my first-application so I am very nervous</h2>
+      <h2>This is my first application so I am very nervous</h2>
       <h2>Do you want to hear something cool?</h2>
-      <button value="clicked" onClick={handleOnClick}>{state}</button>
-      <img src='/coder.jpg'/>
-    </div> );
+      <button value="clicked" onClick={handleOnClick}>
+        {state ? 'Thanks!' : 'Click me!'}
+      </button>
+      <img src="/coder.jpg" alt="coder" />
+    </div>
+  );
 }
 
 export default Sample;
