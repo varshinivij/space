1ef29c0f3f581b1d43eff57a3b863a3d33ffa3a7
Varshini Vijay <varshinivijay05@gmail.com>
2025-07-13 14:29:16 -0700

    changing the reactdom

--- project_builder/src/main.jsx
+++ project_builder/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-root = React.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const root = React.createRoot(document.getElementById('root'));
+root.render(<App />);
