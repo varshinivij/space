c6572af0a5304178224a45981e220ec5edcb246d
Varshini Vijay <varshinivijay05@gmail.com>
2025-07-13 18:32:34 -0700

    some more changes

--- project_builder/src/App.jsx
+++ project_builder/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sample from './Sample';
+import './index.css'
 
 function App() {
   return <Sample/>;
--- project_builder/src/Sample.jsx
+++ project_builder/src/Sample.jsx
@@ -1,26 +1,25 @@
 import './index.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Container } from '@mui/material';
 
-
 function Sample() {
-  const [state, setState] = useState(false);
+  const [state, setState] = useState(null);
+  const [text, setText] = useState('');
 
-  function handleOnClick({ target }) {
-    console.log(`Button was ${target.value}`);
-    setState((prev) => !prev);
-    state ? text = "This is my first application ever" : text ="BYE THEN!"; 
-  }
+  useEffect(
+    !state ? setText("This is my first application ever!") : setText("Bye then!")
+  )
 
   return (
     <div id='main_app'>
-      <Container maxWidth="sm">
-      <h1>Welcome to the Project Builder Application</h1>
-      <h1>Do you want to hear something cool?</h1>
-      <Button variant="contained" value="clicked" onClick={handleOnClick}>
-        {state ? 'Yes' : 'No'}
-      </Button>
-      <img src="/coder.jpg" alt="coder" />
+      <Container maxWidth="fixed" className="my_container">
+        <h1>Welcome to the Project Builder Application</h1>
+        <h1>Do you want to hear something cool?</h1>
+        <Button variant="contained" value="clicked" onClick={setState((prev) => !prev)}>
+          {state ? 'Yes' : 'No'}
+        </Button>
+        <h2>{text}</h2>
+        <img src="/coder.jpg" alt="coder" />
       </Container>
     </div>
   );
