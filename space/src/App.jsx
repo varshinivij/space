717189652ecaf131bd41608d3e327b2310513ae9
Varshini Vijay <varshinivijay05@gmail.com>
2026-03-23 11:21:43 -0700

    changed name

---
+++ space/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App(){
+  const [count, setCount] = React.useState(0);
+  return (
+    <div className ='App'>
+      <main>
+        <text>Welcome to the home page</text>
+      </main>
+    </div>
+  )
+}
+
+export default App;
