3fb62bb77829e768de963bdd8e33fb6abc43d6c1
Varshini Vijay <varshinivijay05@gmail.com>
2026-03-23 19:19:39 -0700

    rename

---
+++ project_builder/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+function App(){
+  const [active, setActive] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const textRef = useRef("");
+  var curr = 0;
+  let arr = [];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (curr > count) {
+      arr.push(e.current.value);
+      curr -= 1;
+    } else {
+      setActive(true);
+    }
+  }
+
+  function getRandomActivity(max) {
+    if (length(arr) < count) return "Error occurred earlier";
+    min = Math.ceil(0);
+    max = Math.floor(max - 1);
+    index = Math.floor(Math.random() * (max - min + 1)) + min;
+    return arr[index];
+  }
+
+  return (
+    <div>
+      !active && <div>
+        <input onChange = {() => setCount(value)} >  Enter number of yours you will study </input>
+        <h1>Enter {count} break activities you would enjoy </h1>
+        curr && <input type="text" ref={textRef}></input>
+        <button type="submit" onSubmit={handleSubmit}> Submit</button>
+      </div>
+
+      arr && <div>
+        <h1> Spin the wheel! </h1>
+        <img src="./wheel.gif"></img>
+        <h2> </h2>
+      </div>
+    </div>
+  );
+}
